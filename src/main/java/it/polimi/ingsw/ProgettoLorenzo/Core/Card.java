f8481de55d0122df4bf3f1026bde59d4cd85fe49
Alessandro Vannoni <alevanno@live.it>
2017-05-10 20:04:32 +0200

    initial Card class
    thought to be subclassed, gets the data from a JsonObject.

---
+++ src/main/java/it/polimi/ingsw/ProgettoLorenzo/Core/Card.java
@@ -0,0 +1,45 @@
+package it.polimi.ingsw.ProgettoLorenzo.Core;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import com.google.gson.*;
+
+/**
+ * Created by Alessandro on 10/05/2017.
+ */
+public abstract class Card extends Action {
+    public final String cardName;  // FIXME make me private
+    private final List<Resources> cardCost = new ArrayList<>();
+
+    private final String cardType;
+    private final String cardPeriod;
+    //private final JsonArray immediateEff;
+    //private final JsonArray permanentEff;
+
+    public Card(JsonObject src) {
+        this.cardName = src.get("name").getAsString();
+        this.cardType = src.get("type").getAsString();
+        this.cardPeriod = src.get("period").getAsString();
+        for (int i=0; i < src.get("cost").getAsJsonArray().size() ; i++) {
+            // FIXME way too many getAsFoo()....
+            this.cardCost.add(new Resources(src.get("cost").getAsJsonArray().get(i).getAsJsonObject()));
+        }
+    }
+
+    public Resources getCardCost() {
+        if (this.cardCost.size() > 1) {
+            System.out.println("You can choose what to pay:");
+            for(Resources item: this.cardCost) {
+               System.out.println(item.toString());
+            }
+            System.out.print("Insert number: ");
+            Scanner in = new Scanner(System.in);
+            return this.cardCost.get(in.nextInt());
+        }
+        return this.cardCost.get(0);
+    }
+}
+
+
