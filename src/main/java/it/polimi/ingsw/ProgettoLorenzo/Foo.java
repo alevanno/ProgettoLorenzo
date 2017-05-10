b7024d10d33474a74be447133f888aab08166ae0
Alessandro Vannoni <alevanno@live.it>
2017-05-10 20:11:46 +0200

    make the Foo class do something remotely useful

--- src/main/java/it/polimi/ingsw/ProgettoLorenzo/Foo.java
+++ src/main/java/it/polimi/ingsw/ProgettoLorenzo/Foo.java
@@ -1,4 +1,16 @@
+package it.polimi.ingsw.ProgettoLorenzo;
+
+import com.google.gson.*;
+import it.polimi.ingsw.ProgettoLorenzo.Core.TerrainCard;
+
+import java.io.FileReader;
+
 public class Foo {
-    public void main(String[] args) {
+    public static void main(String[] args) throws java.io.FileNotFoundException {
+        // FIXME ... find where it looks for the start of the relative path....
+        String filename = "D:\\Dropbox\\Projects\\ProgettoLorenzo\\target\\classes\\it\\polimi\\ingsw\\ProgettoLorenzo\\Core\\cards.json";
+        JsonArray data = new JsonParser().parse(new FileReader(filename)).getAsJsonArray();
+        TerrainCard card = new TerrainCard(data.get(0).getAsJsonObject());
+        card.print();
     }
 }
