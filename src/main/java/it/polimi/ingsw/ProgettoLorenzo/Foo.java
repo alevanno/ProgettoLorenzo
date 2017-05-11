6e5eef99432f6f2cd8ca8b727dcbb18fe85b3c68
Mattia Rizzolo <mattia@mapreri.org>
2017-05-11 13:20:41 +0200

    Load cards.json from a relative path
    Signed-off-by: Mattia Rizzolo <mattia@mapreri.org>

--- src/main/java/it/polimi/ingsw/ProgettoLorenzo/Foo.java
+++ src/main/java/it/polimi/ingsw/ProgettoLorenzo/Foo.java
@@ -6,9 +6,10 @@ import it.polimi.ingsw.ProgettoLorenzo.Core.TerrainCard;
 import java.io.FileReader;
 
 public class Foo {
+
     public static void main(String[] args) throws java.io.FileNotFoundException {
-        // FIXME ... find where it looks for the start of the relative path....
-        String filename = "D:\\Dropbox\\Projects\\ProgettoLorenzo\\target\\classes\\it\\polimi\\ingsw\\ProgettoLorenzo\\Core\\cards.json";
+        ClassLoader classLoader = Foo.class.getClassLoader();
+        String filename = classLoader.getResource("cards.json").getFile();
         JsonArray data = new JsonParser().parse(new FileReader(filename)).getAsJsonArray();
         TerrainCard card = new TerrainCard(data.get(0).getAsJsonObject());
         card.print();
