46e45a1fc287186e1e06968dd1d7754263695fea
Luca Pierri <lukepier95@gmail.com>
2017-05-13 23:39:38 +0200

    added FamilyMember class template

---
+++ src/main/java/it/polimi/ingsw/ProgettoLorenzo/Core/FamilyMember.java
@@ -0,0 +1,19 @@
+package it.polimi.ingsw.ProgettoLorenzo.Core;
+
+
+public class FamilyMember {
+
+    private String skinColor;
+
+    public FamilyMember(String skinColor){
+        this.skinColor = skinColor;
+    }
+
+    public String getSkinColor() {
+        return this.skinColor;
+    }
+
+    public void setSkinColor(String skinColor) {
+        this.skinColor = skinColor;
+    }
+}
