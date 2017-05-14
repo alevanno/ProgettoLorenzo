3de6fbeef42eb9cbd547214fb42bed4cb04a808e
Mattia Rizzolo <mattia@mapreri.org>
2017-05-14 21:03:17 +0200

    define a ResourcesAction extending BaseAction
    Signed-off-by: Mattia Rizzolo <mattia@mapreri.org>

--- src/main/java/it/polimi/ingsw/ProgettoLorenzo/Core/BaseAction.java
+++ src/main/java/it/polimi/ingsw/ProgettoLorenzo/Core/BaseAction.java
@@ -3,10 +3,28 @@ package it.polimi.ingsw.ProgettoLorenzo.Core;
 abstract class BaseAction {
     public final String actionName;
 
-    public BaseAction (String name) {
+    protected BaseAction (String name) {
         this.actionName = name;
     }
 
     public abstract void apply(Player pl);
 }
 
+
+class ResourcesAction extends BaseAction {
+    private Resources op;
+
+    public ResourcesAction (String name, Resources op) {
+        super(name);
+        this.op = op;
+    }
+
+    public void apply(Player pl) {
+        pl.currentRes = pl.currentRes.merge(this.op);
+    }
+
+    @Override
+    public String toString() {
+        return "+ " + this.actionName + ": " + this.op;
+    }
+}
