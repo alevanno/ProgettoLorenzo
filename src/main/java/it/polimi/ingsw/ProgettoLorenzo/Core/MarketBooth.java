647a6c601148aa738af2e4f1a00208579ce928cc
Luca Pierri <lukepier95@gmail.com>
2017-05-14 19:55:15 +0200

    MarketBooth: fix

--- src/main/java/it/polimi/ingsw/ProgettoLorenzo/Core/MarketBooth.java
+++ src/main/java/it/polimi/ingsw/ProgettoLorenzo/Core/MarketBooth.java
@@ -11,13 +11,11 @@ public class MarketBooth {
     private List<Resources> bonus = new ArrayList<>();
     private int councilPrivilege;
     private FamilyMember familyMember;
-    private Council tempCouncil;
 
     public MarketBooth(JsonObject src) throws FileNotFoundException {
         String privilege = "privileges";
         if (src.get(privilege) != null) {
             councilPrivilege = 2;
-            tempCouncil = new Council();
 
         } else {
             councilPrivilege = 0;
@@ -33,10 +31,6 @@ public class MarketBooth {
         return councilPrivilege;
     }
 
-    public Council getTempCouncil() {
-        return tempCouncil;
-    }
-
     public FamilyMember getFamilyMember() {
         return this.familyMember;
     }
@@ -45,7 +39,7 @@ public class MarketBooth {
     public List<Resources> claimSpace(FamilyMember fam) throws FileNotFoundException {
         this.familyMember = fam;
         if (councilPrivilege == 2) {
-            bonus.addAll(tempCouncil.chooseMultiPrivilege(councilPrivilege));
+            bonus.addAll(new Council().chooseMultiPrivilege(councilPrivilege));
         }
         return bonus;
     }
