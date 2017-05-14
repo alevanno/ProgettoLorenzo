42807e24db083e5fa2d0ae552da49590963c1aed
Mattia Rizzolo <mattia@mapreri.org>
2017-05-14 21:14:11 +0200

    Action: build the Action object as a list of BaseActions
    Signed-off-by: Mattia Rizzolo <mattia@mapreri.org>

--- src/main/java/it/polimi/ingsw/ProgettoLorenzo/Core/Action.java
+++ src/main/java/it/polimi/ingsw/ProgettoLorenzo/Core/Action.java
@@ -1,8 +1,27 @@
 package it.polimi.ingsw.ProgettoLorenzo.Core;
 
-/**
- * Created by Alessandro on 10/05/2017.
- */
-public abstract class Action {
+import java.util.ArrayList;
+import java.util.List;
 
+public abstract class Action {
+    private List<BaseAction> actions = new ArrayList<>();
+    protected Player player;
+
+    public void setPlayer(Player p) {
+        this.player = p;
+    }
+
+    public void addAction(BaseAction action) {
+        this.actions.add(action);
+    }
+
+    public void emptyActions() {
+        this.actions.clear();
+    }
+
+    public void apply() {
+        for (int i=0; i < this.actions.size(); i++) {
+            this.actions.get(i).apply(this.player);
+        }
+    }
 }
