8518890a7b8009adf194ce9d981ec0614547c92b
Alessandro Vannoni <alevanno@live.it>
2017-05-14 20:33:42 +0200

    Harvest: almost complete implementation

--- src/main/java/it/polimi/ingsw/ProgettoLorenzo/Core/BonusTile.java
+++ src/main/java/it/polimi/ingsw/ProgettoLorenzo/Core/BonusTile.java
@@ -6,9 +6,11 @@ import com.google.gson.JsonObject;
 import java.io.FileNotFoundException;
 
 public class BonusTile {
-    private Player owner; //forse va istanziato in player, piuttosto che inizializzarlo qui
     private int number;
     private Resources productionRes;
+
+
+
     private Resources harvestRes;
 
     public BonusTile (JsonObject data) throws FileNotFoundException {
@@ -19,6 +21,12 @@ public class BonusTile {
                 .getAsJsonObject());
     }
 
+    public Resources getHarvestRes() {
+        return harvestRes;
+    }
 
+    public Resources getProductionRes() {
+        return productionRes;
+    }
 }
 
---
+++ src/main/java/it/polimi/ingsw/ProgettoLorenzo/Core/Harvest.java
@@ -0,0 +1,56 @@
+package it.polimi.ingsw.ProgettoLorenzo.Core;
+
+import com.google.gson.JsonObject;
+
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Harvest {
+    private FamilyMember mainProduction;
+    private List<FamilyMember> secondaryProduction = new(ArrayList);
+
+    public void claimFamMain(FamilyMember fam) {
+        this.mainProduction = fam;
+        harv(fam.parent, fam.actionValue);
+    }
+    //FIXME non gestisce l'incremento azione con servitori
+
+    public void claimFamSec(FamilyMember fam) {
+        this.secondaryProduction.add(fam);
+        harv(fam.parent, fam.ActionValue);
+    }
+
+    public void harv(Player p, int value) throws FileNotFoundException {
+        Deck tempDeck = new Deck();
+        Resources harvRes = new Resources.ResBuilder().build();
+        if (value < 1) {
+            System.out.println("You need an action value of at least 1");
+            //TODO deve restituire errore al livello superiore
+        } else {
+            //leggere le carte di player che hanno effetto permanente raccolto
+            //escludere quelle di valore superiore all'azione compiuta
+            for (Card i : p.listCards()) {
+                if (i.permanentEff.containsKey("harvest") &&
+                        i.permanentEff.get("harvest").getAsJsonObject().get("value").getAsInt() <= value) {
+                    tempDeck.add(i);
+                }
+            }
+            harvRes.merge(p.BonusT.getHarvestRes());
+            for (Card i : tempDeck) {
+                Resources tmp = Resources.fromJson(i.permanentEff.get("harvest")
+                        .getAsJsonObject().get("resources").getAsJsonObject());
+                harvRes.merge(tmp);
+            }
+            for (Card i : tempDeck) {
+                JsonObject tmp = i.permanentEff.get("harvest").getAsJsonObject().get("councilPrivilege").getAsJsonObject();
+                if (tmp != null) {
+                    harvRes.merge(new Council().choosePrivilege());
+                }
+            }
+
+        }
+
+    }
+}
