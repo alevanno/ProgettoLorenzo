19499b5519f906757d02df907114789c008c36f6
Mattia Rizzolo <mattia@mapreri.org>
2017-05-17 18:53:16 +0200

    Utils: add new getResourceFilePath(), getJsonObject(), getJsonArray() methods
    Signed-off-by: Mattia Rizzolo <mattia@mapreri.org>

--- src/main/java/it/polimi/ingsw/ProgettoLorenzo/Core/Utils.java
+++ src/main/java/it/polimi/ingsw/ProgettoLorenzo/Core/Utils.java
@@ -1,7 +1,12 @@
 package it.polimi.ingsw.ProgettoLorenzo.Core;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Utils {
     public static int returnZeroIfMissing(JsonObject src, String key) {
@@ -13,5 +18,30 @@ public class Utils {
         } else {
             return 0;
         }
+    }
+
+    public static String getResourceFilePath(String filename) {
+        ClassLoader classLoader = Utils.class.getClassLoader();
+        return classLoader.getResource(filename).getFile();
+    }
+
+    public static JsonObject getJsonObject(String filename) {
+        JsonObject data = new JsonObject();
+        try (FileReader f = new FileReader(getResourceFilePath(filename))) {
+            data = new JsonParser().parse(f).getAsJsonObject();
+        } catch (IOException e) {
+            System.err.println(String.format("File %s not found", filename));
+        }
+        return data;
+    }
+
+    public static JsonArray getJsonArray(String filename) {
+        JsonArray data = new JsonArray();
+        try (FileReader f = new FileReader(getResourceFilePath(filename))) {
+            data = new JsonParser().parse(f).getAsJsonArray();
+        } catch (IOException e) {
+            System.err.println(String.format("File %s not found", filename));
+        }
+        return data;
     }
 }
