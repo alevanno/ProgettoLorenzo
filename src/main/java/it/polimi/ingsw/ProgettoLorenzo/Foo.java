9c933d5a2077ad66f2c345a61acebf0570ba1d3d
Mattia Rizzolo <mattia@mapreri.org>
2017-05-18 02:39:22 +0200

    create a quite interestingly working Game class
    Signed-off-by: Mattia Rizzolo <mattia@mapreri.org>

--- src/main/java/it/polimi/ingsw/ProgettoLorenzo/Foo.java
+++ src/main/java/it/polimi/ingsw/ProgettoLorenzo/Foo.java
@@ -1,59 +1,10 @@
 package it.polimi.ingsw.ProgettoLorenzo;
 
-import com.google.gson.*;
 import it.polimi.ingsw.ProgettoLorenzo.Core.*;
 
 public class Foo {
 
     public static void main(String[] args) {
-        JsonArray data = Utils.getJsonArray("cards.json");
-        Card test = new Card(data.get(0).getAsJsonObject());
-        Player p1 = new Player("Ciccio", "Red");
-        JsonArray databt = Utils.getJsonArray("bonusTile.json");
-        BonusTile testbt = new BonusTile(databt.get(0).getAsJsonObject());
-        p1.setBonusTile(testbt);
-        System.out.println("foo " + p1.bonusT.getHarvestRes().toString());
-
-        p1.addCard(test);
-        int val = 5;
-        Harvest h = new Harvest();
-        h.harv(p1, val);
+        Game game = new Game();
     }
-        /*ClassLoader classLoader = Foo.class.getClassLoader();
-        String filename = classLoader.getResource("cards.json").getFile();
-        JsonArray data = new JsonParser().parse(new FileReader(filename))
-                .getAsJsonArray();
-        Card test = new Card(data.get(0).getAsJsonObject());
-        test.actionBuilder();
-        Player player = new Player("Pino", "Red");
-        test.setPlayer(player);
-        player.famMembersBirth();
-        test.apply();
-        System.out.println(player.currentRes);
-        Council counc = new Council();
-        counc.claimSpace(player.getAvailableFamMembers().get(0));
-        counc.setPlayer(player);
-        counc.apply();
-        System.out.println(player.currentRes);
-        Market market = new Market();
-        MarketBooth booth = market.getBooths().get(3); //try with the MultiPriv
-        booth.setPlayer(player);
-        booth.claimSpace(player.getAvailableFamMembers().get(1));
-        booth.apply();
-        System.out.println(player.currentRes);
-        Deck tmpList = new Deck();
-        tmpList.add(test);
-        tmpList.add(test);
-        tmpList.add(test);
-        tmpList.add(test);
-        Tower tower = new Tower(1, tmpList);
-        Floor testFloor = tower.getFloors().get(0);
-        testFloor.claimFloor(player.getAvailableFamMembers().get(2));
-        testFloor.setPlayer(player);
-        testFloor.apply();
-        System.out.println(player.currentRes);
-
-    }*/
-
-
 }
---
+++ src/main/java/it/polimi/ingsw/ProgettoLorenzo/Game.java
@@ -0,0 +1,41 @@
+package it.polimi.ingsw.ProgettoLorenzo;
+
+import com.google.gson.JsonObject;
+import it.polimi.ingsw.ProgettoLorenzo.Core.*;
+
+
+public class Game {
+    private Board board;
+    private Deck unhandledCards = new Deck();
+    private Player player = new Player("Test player", "red");
+
+    public Game() {
+        this.loadCards();
+        this.turn();
+    }
+
+    private void resetBoard() {
+        this.board = new Board(this.unhandledCards);
+    }
+
+    private void loadCards() {
+        // FIXME ....lol......
+        JsonObject data0 = Utils.getJsonArray("cards.json")
+                .get(0).getAsJsonObject();
+        this.unhandledCards.add(new Card(data0));
+        this.unhandledCards.add(new Card(data0));
+        this.unhandledCards.add(new Card(data0));
+        this.unhandledCards.add(new Card(data0));
+    }
+
+    private void turn() {
+        this.resetBoard();
+        this.player.famMembersBirth();
+        this.round();
+    }
+
+    private void round() {
+        this.board.towers.get(0).getFloors().get(0).claimFloor(this.player
+                .getAvailableFamMembers().get(0));
+    }
+}
