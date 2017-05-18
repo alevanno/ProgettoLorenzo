43cf4d603a20b1bacf28ef0a92422e487d1ca458
Mattia Rizzolo <mattia@mapreri.org>
2017-05-18 02:28:03 +0200

    Card: introduce a stub immediateActionBuilder() using NullAction
    Signed-off-by: Mattia Rizzolo <mattia@mapreri.org>

--- src/main/java/it/polimi/ingsw/ProgettoLorenzo/Core/Card.java
+++ src/main/java/it/polimi/ingsw/ProgettoLorenzo/Core/Card.java
@@ -79,6 +79,12 @@ public class Card extends Action {
         );
     }
 
+    public void immediateActionBuilder(Player player) {
+        this.addAction(
+                new NullAction();
+        );
+    }
+
         // FIXME this has to create a BaseAction's for all the actions
         // we're interested in.
 
