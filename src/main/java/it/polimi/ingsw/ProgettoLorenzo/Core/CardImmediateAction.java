60406b4f4683c443ceafc67ef2a382224d8b826f
Luca Pierri <lukepier95@gmail.com>
2017-05-18 13:46:46 +0200

    CardImmediateAction: implementation

--- src/main/java/it/polimi/ingsw/ProgettoLorenzo/Core/CardImmediateAction.java
+++ src/main/java/it/polimi/ingsw/ProgettoLorenzo/Core/CardImmediateAction.java
@@ -1,9 +1,52 @@
 package it.polimi.ingsw.ProgettoLorenzo.Core;
 
 
-public class CardImmediateAction {
+import com.google.gson.JsonObject;
 
-    public CardImmediateAction(Card card) {
+import java.util.Set;
 
+public class CardImmediateAction extends Action {
+
+    public CardImmediateAction(Card card, Player pl) {
+
+        if (card.immediateEff.containsKey("resources")) {
+            Resources tmp = Resources.fromJson(card.immediateEff.get("resources").getAsJsonObject());
+            this.addAction(new ResourcesAction("Resources", tmp, pl));
+            System.out.println("ImmediateAction: Card " + card.getCardName() + " gave " + tmp.toString());
+        }
+
+        if (card.immediateEff.containsKey("councilPrivilege")) {
+            int priv = card.immediateEff.get("councilPrivilege").getAsInt();
+            Set<Resources> privRes = (new Council().chooseMultiPrivilege(priv));
+            for (Resources r : privRes) {
+                this.addAction(new ResourcesAction(
+                        "ImmActCouncilPrivilege", r, pl));
+                System.out.println("Production: Card " + card.getCardName() + " gave " + r.toString());
+        }
+
+            if (card.immediateEff.containsKey("production")) {
+                int value = card.immediateEff.get("production").getAsInt();
+                new Production().prod(pl, value);
+            }
+            //TODO to handle pickCard we have first to discuss the cardCostHandling
+            if(card.immediateEff.containsKey("pickCard")) {
+
+            }
+
+            if(card.immediateEff.containsKey("multiplier")) {
+                Resources prodRes = new Resources.ResBuilder().build();
+                JsonObject mult = card.immediateEff.get("multiplier").getAsJsonObject();
+                String tmpType = mult.get("type").getAsString();
+                Resources tmpRes = Resources.fromJson(mult.get("bonus").getAsJsonObject());
+                int count = 0;
+                for (Card c : pl.listCards()) {
+                    if (c.cardType.equals(tmpType)) { count++; }
+                }
+                prodRes.merge(tmpRes.multiplyRes(count));
+                this.addAction(new ResourcesAction(
+                        "ProdMultiplier", tmpRes.multiplyRes(count), pl));
+                System.out.println("Production: Multiplier Card " + card.getCardName() + " gave " + tmpRes.toString());
+            }
+        }
     }
 }
