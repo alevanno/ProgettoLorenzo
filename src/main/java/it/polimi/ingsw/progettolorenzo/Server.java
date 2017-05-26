2d568c8257a52de09d7fd9ff6e9c53b8dc55a709
Mattia Rizzolo <mattia@mapreri.org>
2017-05-26 18:14:51 +0200

    Server: fix indentation
    Signed-off-by: Mattia Rizzolo <mattia@mapreri.org>

--- src/main/java/it/polimi/ingsw/progettolorenzo/Server.java
+++ src/main/java/it/polimi/ingsw/progettolorenzo/Server.java
@@ -11,13 +11,11 @@ import java.util.List;
 import java.util.Scanner;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class Server {
     private final Logger log = Logger.getLogger(this.getClass().getName());
-    private Game game;
 
     public void startServer() throws IOException {
         ExecutorService executor = Executors.newCachedThreadPool();
@@ -26,28 +24,26 @@ public class Server {
                 Config.server.get("bind").getAsString()
             );
         ServerSocket serverSocket = new ServerSocket(port, 0, address);
-        System.out.println("Server socket ready on port: " + port);
-        System.out.println("Server ready");
+        log.info("Server socket ready on port: " + port);
+        log.info("Server ready");
         List<Player> listPlayers = new ArrayList<>();
-            while (true) {
-                try {
-                    Socket socket = serverSocket.accept();
-                    log.info("new Client connection");
-                    Scanner socketIn = new Scanner(socket.getInputStream());
-                    String name = socketIn.nextLine();
-                    String colour = socketIn.nextLine();
-                    Player player = new Player(name, colour, socket);
-                    listPlayers.add(player);
-                } catch (IOException e) {
-                    log.log(Level.SEVERE, e.getMessage(), e);
-                    break;
-                }
-                if (listPlayers.size() == 2) {
-                    break;
-                }
-
-
+        while (true) {
+            try {
+                Socket socket = serverSocket.accept();
+                log.info("new Client connection");
+                Scanner socketIn = new Scanner(socket.getInputStream());
+                String name = socketIn.nextLine();
+                String colour = socketIn.nextLine();
+                Player player = new Player(name, colour, socket);
+                listPlayers.add(player);
+            } catch (IOException e) {
+                log.log(Level.SEVERE, e.getMessage(), e);
+                break;
             }
+            if (listPlayers.size() == 2) {
+                break;
+            }
+        }
         executor.submit(new Game(listPlayers));
         executor.shutdown();
         serverSocket.close();
