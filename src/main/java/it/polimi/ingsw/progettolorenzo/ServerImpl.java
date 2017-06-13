f6970f52141203016d39a4cbdf4a87e8254d985e
Mattia Rizzolo <mattia@mapreri.org>
2017-06-13 16:01:22 +0200

    Server: move the "waiting for connections" stuff in a separated thread
    this way RMI clients can play alone, and a lot of new cool things!
    
    Signed-off-by: Mattia Rizzolo <mattia@mapreri.org>

--- src/main/java/it/polimi/ingsw/progettolorenzo/ServerImpl.java
+++ src/main/java/it/polimi/ingsw/progettolorenzo/ServerImpl.java
@@ -17,6 +17,59 @@ import java.util.concurrent.Executors;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+class ConnectionService implements Runnable {
+    private final Logger log = Logger.getLogger(this.getClass().getName());
+    private final ServerImpl server;
+
+    public ConnectionService(ServerImpl server) {
+        this.server = server;
+    }
+
+    @Override
+    public void run() {
+        try {
+            this.runC();
+        } catch (IOException e) {
+            log.log(Level.SEVERE, e.getMessage(), e);
+        }
+    }
+
+    private void runC() throws IOException {
+        // RMI
+        int port = Config.Server.rmi.get("port").getAsInt();
+        LocateRegistry.createRegistry(port);
+        Registry reg = LocateRegistry.getRegistry(port);
+        reg.rebind("Lorenzo", this.server);
+        log.info("RMI server ready");
+
+        // socket
+        port = Config.Server.socket.get("port").getAsInt();
+        InetAddress address = InetAddress.getByName(
+            Config.Server.socket.get("bind").getAsString()
+        );
+        ServerSocket serverSocket = null;
+        try {
+            serverSocket = new ServerSocket(port, 0, address);
+            log.info("Socket server ready on " + address + ", port: " + port);
+            while (true) {
+                try {
+                    Socket socket = serverSocket.accept();
+                    log.info("new SocketClient connection");
+                    server.addPlayer(socket);
+                } catch (IOException e) {
+                    log.log(Level.SEVERE, e.getMessage(), e);
+                    throw e;
+                }
+            }
+        } finally {
+            if (serverSocket != null) {
+                serverSocket.close();
+            }
+        }
+    }
+
+}
+
 public class ServerImpl extends UnicastRemoteObject implements Server {
     private transient final Logger log = Logger.getLogger(this.getClass().getName());
     private int playersNum;
@@ -28,65 +81,53 @@ public class ServerImpl extends UnicastRemoteObject implements Server {
     }
 
     private void startServer() throws IOException {
-        ExecutorService executor = Executors.newCachedThreadPool();
-        // RMI
-        int port = Config.Server.rmi.get("port").getAsInt();
-        LocateRegistry.createRegistry(port);
-        Registry reg = LocateRegistry.getRegistry(port);
-        reg.rebind("Lorenzo", this);
-        log.info("ServerImpl ready");
-        // socket
-        port = Config.Server.socket.get("port").getAsInt();
-        InetAddress address = InetAddress.getByName(
-            Config.Server.socket.get("bind").getAsString()
-        );
-        ServerSocket serverSocket = null;
-        try {
-            serverSocket = new ServerSocket(port, 0, address);
-            log.info("ServerImpl socket ready on " + address + ", port: " + port);
-            while (true) {
+        ExecutorService games = Executors.newCachedThreadPool();
+        ExecutorService connections = Executors.newSingleThreadExecutor();
+        connections.submit(new ConnectionService(this));
+        while (true) {
+            synchronized (this.players) {
                 try {
-                    Socket socket = serverSocket.accept();
-                    log.info("new SocketClient connection");
-                    this.addPlayer(socket);
-                } catch (IOException e) {
-                    log.log(Level.SEVERE, e.getMessage(), e);
-                    throw e;
-                }
-                if (this.players.size() == this.playersNum) {
+                    log.warning("players waiting…");
+                    this.players.wait();
+                    games.submit(
+                        new Game(this.players, this.personalBonusBoards)
+                    );
+                    games.shutdown();  // TODO support multiple games
                     break;
+                } catch (InterruptedException e) {
+                    log.log(Level.SEVERE, e.getMessage(), e);
                 }
             }
-            executor.submit(new Game(this.players, this.personalBonusBoards));
-            executor.shutdown();
-        } finally {
-            if (serverSocket != null) {
-                serverSocket.close();
+        }
+
+    }
+
+    private void addPlayer(Player player) {
+        synchronized (this.players) {
+            this.players.add(player);
+            if (this.players.size() == 1) {
+                this.firstPlayer(player);
+            }
+            if (this.players.size() == this.playersNum) {
+                this.players.notify();
             }
         }
     }
 
     @Override
-    public synchronized void addPlayer(
+    public void addPlayer(
         String name, String colour, RmiClient rmiClient
     ) throws RemoteException {
         Player player = new Player(name, colour, rmiClient);
-        this.players.add(player);
-        if (this.players.size() == 1) {
-            this.firstPlayer(player);
-        }
+        this.addPlayer(player);
     }
 
-    public synchronized void addPlayer(Socket socket) throws IOException {
+    public void addPlayer(Socket socket) throws IOException {
         Scanner socketIn = new Scanner(socket.getInputStream());
         String name = socketIn.nextLine();
         String colour = socketIn.nextLine();
         Player player = new Player(name, colour, socket);
-        this.players.add(player);
-        if (this.players.size() == 1) {
-            this.firstPlayer(player);
-        }
-
+        this.addPlayer(player);
     }
 
     private void firstPlayer(Player pl) {
