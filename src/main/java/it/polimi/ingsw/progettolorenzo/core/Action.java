c77232aaf6525c441f8deed4e4bfbe09884ab7dd
Luca Pierri <lukepier95@gmail.com>
2017-06-14 11:01:46 +0200

    fast fixes

--- src/main/java/it/polimi/ingsw/progettolorenzo/core/Action.java
+++ src/main/java/it/polimi/ingsw/progettolorenzo/core/Action.java
@@ -6,7 +6,7 @@ import java.util.logging.Logger;
 
 public abstract class Action {
     private final Logger log = Logger.getLogger(this.getClass().getName());
-    public List<BaseAction> actions = new ArrayList<>();
+    private List<BaseAction> actions = new ArrayList<>();
 
     public void addAction(BaseAction action) {
         this.actions.add(action);
