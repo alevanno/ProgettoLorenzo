793b4bd298d0130ffd5966cefba422c39b31bb82
Mattia Rizzolo <mattia@mapreri.org>
2017-06-20 16:21:41 +0200

    drop unused InsuffActionValException
    Signed-off-by: Mattia Rizzolo <mattia@mapreri.org>

--- src/main/java/it/polimi/ingsw/progettolorenzo/core/InsuffActionValException.java
+++
@@ -1,7 +0,0 @@
-package it.polimi.ingsw.progettolorenzo.core;
-
-public class InsuffActionValException extends Exception {
-    public InsuffActionValException(String message) {
-        super(message);
-    }
-}
