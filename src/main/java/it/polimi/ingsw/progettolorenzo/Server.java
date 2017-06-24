963ffac0f5e88d953a6d5f3bd5e0b9d495b3b53c
Mattia Rizzolo <mattia@mapreri.org>
2017-06-24 15:20:42 +0200

    Server: add a stupid @FunctionalInterface annotation…
    sonar stuff
    
    Signed-off-by: Mattia Rizzolo <mattia@mapreri.org>

--- src/main/java/it/polimi/ingsw/progettolorenzo/Server.java
+++ src/main/java/it/polimi/ingsw/progettolorenzo/Server.java
@@ -5,6 +5,9 @@ import it.polimi.ingsw.progettolorenzo.client.RmiClient;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
+// annotating @FunctionalInterface here is ridiculous, but let's do it
+// to make sonar a tad happier, it should change nothing anyway
+@FunctionalInterface
 public interface Server extends Remote {
     void addPlayer(String n, String c, RmiClient r) throws RemoteException;
 }
