9d2ffe778edd12f3d05412e0f8ef8c6a25aeef43
Mattia Rizzolo <mattia@mapreri.org>
2017-07-10 14:45:15 +0200

    add a TestGui class, to help with testing the gui
    Signed-off-by: Mattia Rizzolo <mattia@mapreri.org>

---
+++ src/main/java/it/polimi/ingsw/progettolorenzo/client/inf/gui/TestGui.java
@@ -0,0 +1,25 @@
+package it.polimi.ingsw.progettolorenzo.client.inf.gui;
+
+import it.polimi.ingsw.progettolorenzo.core.Utils;
+
+import java.io.*;
+import java.util.stream.Collectors;
+
+/**
+ * test class, reads a file containing a JSON like the one sent by the
+ * server and displays the board.
+ * Used to debug the gui.
+ */
+public class TestGui {
+    public static void main(String[] args) throws Exception {
+        GuiInterface g = new GuiInterface();
+        FileInputStream fis = new FileInputStream(
+            Utils.getResourceFilePath("testJson")
+        );
+        InputStreamReader in = new InputStreamReader(fis, "UTF-8");
+        String result = new BufferedReader(in)
+            .lines().collect(Collectors.joining("\n"));
+        g.printLine(result);
+    }
+
+}
